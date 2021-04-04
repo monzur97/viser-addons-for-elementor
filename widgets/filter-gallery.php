38fbb45aafd6aa724b041bc59522a5c82af786c0
monzur97 <monzurviserx@gmail.com>
2021-04-04 15:57:03 +0600

    button center

--- widgets/filter-gallery.php
+++ widgets/filter-gallery.php
@@ -103,8 +103,41 @@ class Filter_Gallery extends \Elementor\Widget_Base {
 		$settings = $this->get_settings_for_display();
 
         ?>
+        <div class="container text-center">
+            <div class="row">
+                <div class="col-12@sm filters-group-wrap">
+                    <div class="filters-group m-auto">
+                        <div class="btn-group filter-options">
+                            <button class="btn btn--primary" data-group="space">Space</button>
+                            <button class="btn btn--primary" data-group="nature">Nature</button>
+                            <button class="btn btn--primary" data-group="animal">Animal</button>
+                            <button class="btn btn--primary" data-group="city">City</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
 
-
+        <div class="container">
+            <div id="grid" class="row my-shuffle-container">
+                <figure class="col-3@xs col-4@sm col-3@md picture-item" data-groups='["nature"]' data-date-created="2017-04-30" data-title="Lake Walchen">
+                    <div class="picture-item__inner">
+                        <div class="aspect aspect--16x9">
+                            <div class="aspect__inner">
+                                <img src="https://images.unsplash.com/photo-1493585552824-131927c85da2?ixlib=rb-0.3.5&auto=format&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=284&h=160&fit=crop&s=6ef0f8984525fc4500d43ffa53fe8190" srcset="https://images.unsplash.com/photo-1493585552824-131927c85da2?ixlib=rb-0.3.5&auto=format&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=284&h=160&fit=crop&s=6ef0f8984525fc4500d43ffa53fe8190 1x, https://images.unsplash.com/photo-1493585552824-131927c85da2?ixlib=rb-0.3.5&auto=format&q=55&fm=jpg&dpr=2&crop=entropy&cs=tinysrgb&w=284&h=160&fit=crop&s=6ef0f8984525fc4500d43ffa53fe8190 2x" alt="A deep blue lake sits in the middle of vast hills covered with evergreen trees" />
+                            </div>
+                        </div>
+                        <div class="picture-item__details">
+                            <figcaption class="picture-item__title"><a href="https://unsplash.com/photos/zshyCr6HGw0" target="_blank" rel="noopener">Lake Walchen</a></figcaption>
+                            <p class="picture-item__tags hidden@xs">nature</p>
+                        </div>
+                    </div>
+                </figure>
+            
+                <div class="col-1@sm col-1@xs my-sizer-element"></div>
+            </div>
+        </div>
+        <script src='https://unpkg.com/shufflejs@5'></script>
         <?php
 
 	}
