db59a5b51087ef1e5b48124f6aea05a395f803d5
monzur97 <monzurviserx@gmail.com>
2021-04-04 13:28:52 +0600

    initial setup file

---
+++ viser-addons-for-elementor.php
@@ -0,0 +1,316 @@
+<?php
+/**
+ * Plugin Name: VISER Addons for Elementor
+ * Description: The VISER plugin you install after Elementor!
+ * Plugin URI:  https://profiles.wordpress.org/monzuralam
+ * Version:     1.0.0
+ * Author:      Monzur Alam
+ * Author URI:  https://profiles.wordpress.org/monzuralam
+ * Text Domain: viser-addons-for-elementor
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly.
+}
+
+/**
+ * Main Elementor Test Extension Class
+ *
+ * The main class that initiates and runs the plugin.
+ *
+ * @since 1.0.0
+ */
+final class VISER_Elementor {
+
+	/**
+	 * Plugin Version
+	 *
+	 * @since 1.0.0
+	 *
+	 * @var string The plugin version.
+	 */
+	const VERSION = '1.0.0';
+
+	/**
+	 * Minimum Elementor Version
+	 *
+	 * @since 1.0.0
+	 *
+	 * @var string Minimum Elementor version required to run the plugin.
+	 */
+	const MINIMUM_ELEMENTOR_VERSION = '2.0.0';
+
+	/**
+	 * Minimum PHP Version
+	 *
+	 * @since 1.0.0
+	 *
+	 * @var string Minimum PHP version required to run the plugin.
+	 */
+	const MINIMUM_PHP_VERSION = '7.0';
+
+	/**
+	 * Instance
+	 *
+	 * @since 1.0.0
+	 *
+	 * @access private
+	 * @static
+	 *
+	 * @var Elementor_Test_Extension The single instance of the class.
+	 */
+	private static $_instance = null;
+
+	/**
+	 * Instance
+	 *
+	 * Ensures only one instance of the class is loaded or can be loaded.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @access public
+	 * @static
+	 *
+	 * @return Elementor_Test_Extension An instance of the class.
+	 */
+	public static function instance() {
+
+		if ( is_null( self::$_instance ) ) {
+			self::$_instance = new self();
+		}
+		return self::$_instance;
+
+	}
+
+	/**
+	 * Constructor
+	 *
+	 * @since 1.0.0
+	 *
+	 * @access public
+	 */
+	public function __construct() {
+		add_action('init', [ $this, 'i18n'] );
+		add_action( 'plugins_loaded', [ $this, 'init' ] );
+	}
+
+	/**
+	 * Load Textdomain
+	 *
+	 * Load plugin localization files.
+	 *
+	 * Fired by `init` action hook.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @access public
+	 */
+	public function i18n() {
+
+		load_plugin_textdomain( 'viser-addons-for-elementor',FALSE, basename( dirname(__FILE__)).'/languages/' );
+
+	}
+
+
+	/**
+	 * Initialize the plugin
+	 *
+	 * Load the plugin only after Elementor (and other plugins) are loaded.
+	 * Load the files required to run the plugin.
+	 *
+	 * Fired by `plugins_loaded` action hook.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @access public
+	 */
+	public function init() {
+		// category
+		add_action('elementor/init', [$this,'init_category'] );
+	
+		// Check if Elementor installed and activated
+		if ( ! did_action( 'elementor/loaded' ) ) {
+			add_action( 'admin_notices', [ $this, 'admin_notice_missing_main_plugin' ] );
+			return;
+		}
+
+		// Check for required Elementor version
+		if ( ! version_compare( ELEMENTOR_VERSION, self::MINIMUM_ELEMENTOR_VERSION, '>=' ) ) {
+			add_action( 'admin_notices', [ $this, 'admin_notice_minimum_elementor_version' ] );
+			return;
+		}
+
+		// Check for required PHP version
+		if ( version_compare( PHP_VERSION, self::MINIMUM_PHP_VERSION, '<' ) ) {
+			add_action( 'admin_notices', [ $this, 'admin_notice_minimum_php_version' ] );
+			return;
+		}
+
+		// Register Widget Styles
+		add_action( 'elementor/frontend/after_enqueue_styles', [ $this, 'widget_styles' ] );
+
+		// Register Widget Scripts
+		add_action( 'elementor/frontend/after_register_scripts', [ $this, 'widget_scripts' ] );
+
+		// Add Plugin actions
+		add_action( 'elementor/widgets/widgets_registered', [ $this, 'init_widgets' ] );
+		add_action( 'elementor/controls/controls_registered', [ $this, 'init_controls' ] );
+
+		// shortcode support 
+		add_filter( 'the_title', 'do_shortcode' );
+
+	}
+
+	/**
+	 * Init Widgets
+	 *
+	 * Include widgets files and register them
+	 *
+	 * @since 1.0.0
+	 *
+	 * @access public
+	 */
+	public function init_widgets() {
+
+		// Include Widget files
+		// require_once( __DIR__ . '/widgets/fx-posts.php' );
+
+		// Register widget
+		// \Elementor\Plugin::instance()->widgets_manager->register_widget_type( new \Fx_Posts() );
+
+	}
+
+	/**
+	 * Init Category Section
+	 * 
+	 * @since 1.0.0
+	 */
+	public function init_category(){
+		Elementor\Plugin::instance()->elements_manager->add_category(
+			'viser-addons',
+			[
+				'title'	=>	'VISER ADDONS'	
+			],
+			1
+		);
+	}
+
+	/**
+	 * Init Controls
+	 *
+	 * Include controls files and register them
+	 *
+	 * @since 1.0.0
+	 *
+	 * @access public
+	 */
+	public function init_controls() {
+
+		// Include Control files
+		// require_once( __DIR__ . '/controls/test-control.php' );
+
+		// Register control
+		// \Elementor\Plugin::$instance->controls_manager->register_control( 'control-type-', new \Test_Control() );
+
+	}
+	
+	/**
+	 * Admin notice
+	 *
+	 * Warning when the site doesn't have Elementor installed or activated.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @access public
+	 */
+	public function admin_notice_missing_main_plugin() {
+
+		if ( isset( $_GET['activate'] ) ) unset( $_GET['activate'] );
+
+		$message = sprintf(
+			/* translators: 1: Plugin name 2: Elementor */
+			esc_html__( '"%1$s" requires "%2$s" to be installed and activated.', 'viser-addons-for-elementor' ),
+			'<strong>' . esc_html__( 'Fx Elementor', 'viser-addons-for-elementor' ) . '</strong>',
+			'<strong>' . esc_html__( 'Elementor', 'viser-addons-for-elementor' ) . '</strong>'
+		);
+
+		printf( '<div class="notice notice-warning is-dismissible"><p>%1$s</p></div>', $message );
+
+	}
+
+	/**
+	 * Admin notice
+	 *
+	 * Warning when the site doesn't have a minimum required Elementor version.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @access public
+	 */
+	public function admin_notice_minimum_elementor_version() {
+
+		if ( isset( $_GET['activate'] ) ) unset( $_GET['activate'] );
+
+		$message = sprintf(
+			/* translators: 1: Plugin name 2: Elementor 3: Required Elementor version */
+			esc_html__( '"%1$s" requires "%2$s" version %3$s or greater.', 'viser-addons-for-elementor' ),
+			'<strong>' . esc_html__( 'FX Elementor', 'viser-addons-for-elementor' ) . '</strong>',
+			'<strong>' . esc_html__( 'Elementor', 'viser-addons-for-elementor' ) . '</strong>',
+			 self::MINIMUM_ELEMENTOR_VERSION
+		);
+
+		printf( '<div class="notice notice-warning is-dismissible"><p>%1$s</p></div>', $message );
+
+	}
+
+	/**
+	 * Admin notice
+	 *
+	 * Warning when the site doesn't have a minimum required PHP version.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @access public
+	 */
+	public function admin_notice_minimum_php_version() {
+
+		if ( isset( $_GET['activate'] ) ) unset( $_GET['activate'] );
+
+		$message = sprintf(
+			/* translators: 1: Plugin name 2: PHP 3: Required PHP version */
+			esc_html__( '"%1$s" requires "%2$s" version %3$s or greater.', 'viser-addons-for-elementor' ),
+			'<strong>' . esc_html__( 'Fx Elementor', 'viser-addons-for-elementor' ) . '</strong>',
+			'<strong>' . esc_html__( 'PHP', 'viser-addons-for-elementor' ) . '</strong>',
+			 self::MINIMUM_PHP_VERSION
+		);
+
+		printf( '<div class="notice notice-warning is-dismissible"><p>%1$s</p></div>', $message );
+
+	}
+
+	/**
+	 * Custom Css
+	 * 
+	 * @since 1.0.0
+	 * 
+	 * @access public 
+	 */
+	public function widget_styles(){
+		wp_enqueue_style('fx-css',plugins_url('assets/css/style.css',__FILE__));
+	}
+
+	/**
+	 * Custom JS
+	 * 
+	 * @since 1.0.0
+	 * 
+	 * @access public 
+	 */
+	public function widget_scripts(){
+		wp_enqueue_script('fxscript',plugins_url('assets/js/script.js',__FILE__),array('jquery'),time(),true);
+	}
+
+
+}
+
+VISER_Elementor::instance();
